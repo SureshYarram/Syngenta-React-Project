3a8d93e3cc16811bfb7ca48e7c81caebf1e9b80c
SureshYarram <sureshchowdary540@gmail.com>
2022-05-29 20:01:20 +0530

    adding divs

---
+++ src/Components/Home.jsx
@@ -0,0 +1,32 @@
+
+
+
+export const Home = ()=>{
+ 
+
+
+
+
+    return(
+        <>
+      <h1>BALLOON GAME</h1>
+      <div className="mainDiv">
+            <div className="left">
+            
+                 <div> </div>
+           
+            </div>
+            <div className="right">
+             
+                  <div></div>
+           
+            </div>
+            <div className="input">
+                <input type="text" placeholder="Enter Your Ballon Number"   /> <br />
+                <button>Shoot</button>
+            </div>
+        </div>
+
+        </>
+    )
+}
